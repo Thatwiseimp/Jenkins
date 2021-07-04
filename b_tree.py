0e607b4e0ff00a4dc279e1b1456d6f03f331a28b
sidharth <sidharthram360@gmail.com>
2021-07-04 05:26:16 +0000

    py code

---
+++ b_tree.py
@@ -0,0 +1,63 @@
+
+class Tree():
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+    def assignment(self,data):
+        if self.data==data:
+            return
+        else:
+            if data<self.data:
+                if self.left:
+                    self.left.assignment(data)
+                else:
+                    self.left=Tree(data)
+            else:
+                if self.right:
+                    self.right.assignment(data)
+                else:
+                    self.right=Tree(data)
+
+
+    def inOrderTraversal(self):
+        elements=[]
+        if self.left:
+            elements += self.left.inOrderTraversal()
+
+        elements.append(self.data)
+        if self.right:
+            elements += self.right.inOrderTraversal()
+        return elements
+
+    def search(self,value):
+        if self.data==value:
+            return True
+        else:
+            if value<self.data:
+                if self.left:
+                    return self.left.search(value)
+                else:
+                    return False
+            else:
+                if self.right:
+                    return self.right.search(value)
+                else:
+                    return False
+
+
+def build_tree(elements):
+    root=Tree(elements[0])
+    for i in range(1,len(elements)):
+        root.assignment(elements[i])
+    return root
+
+
+
+if __name__=='__main__':
+    numbers=list(input().split())
+    numbers_tree=build_tree(numbers)
+    id=int(input())
+    print(numbers_tree.inOrderTraversal())
+    print(numbers_tree.search(id))
